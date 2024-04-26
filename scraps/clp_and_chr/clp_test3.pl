e15328d1660dd78ac196c5c01415c3b09c5ddb66
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-04-26 11:51:33 +0200

    migrated stuff to FOL_solvers, and reorg

--- scraps/clp_and_chr/clp_test3.pl
+++
@@ -1,7 +0,0 @@
-main :-
-	format("X: ~w~nY: ~w~n", [X, Y]),
-	f([0, 0, X, Y]).
-
-f(Lin) :-
-	Lin = [_, _ | Rest],
-	format("Rest: ~w~n", [Rest]).
--- scraps/ledger2/action_verbs.pl
+++
@@ -1,1 +0,0 @@
-well, we can support extracting action verbs from xml, but the main format will be rdf and directly sent from plugin.
